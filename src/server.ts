4d5b2bd00e0ef63f7360d11d0762bccd6fe50982
Md.Atiqur Rahman <atiq9se@gmail.com>
2025-07-25 15:08:29 +0600

    library mangament node server setup

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import {Server} from "http";
+
+import mongoose from "mongoose";
+import app from "./app";
+
+let server: Server;
+const PORT = 5000;
+
+async function main(){
+     try{
+        await mongoose.connect('mongodb+srv://atiqse999:0LusSV6IVq6J4QTh@cluster0.grvck.mongodb.net/advanced-note-app?retryWrites=true&w=majority&appName=Cluster0');
+        console.log("connected to mongodb using mongoose")
+        server = app.listen(PORT, ()=>{
+            console.log(`App is listening on port ${PORT}`);
+        });
+
+     } catch(error){
+        console.log(error);
+     }
+}
+main()
